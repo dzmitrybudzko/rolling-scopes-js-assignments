0de904154908c9cfd99c2843fce653d0e8ef0ef5
Dzmitry Budzko1 <dzmitry_budzko1@epam.com>
2016-03-11 17:45:01 +0300

    date

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   return date.getMonth() == 1;
+   return new Date(date.getFullYear(),1,29).getMonth() == 1;
 }
 
 
