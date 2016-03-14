1aed63a3203952b33abac10140c7cc424797acfc
Dzmitry Budzko1 <dzmitry_budzko1@epam.com>
2016-03-14 15:32:14 +0300

    arrays

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -557,8 +557,11 @@ function selectMany(arr, childrenSelector) {
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
 function getElementByIndexes(arr, indexes) {
-    throw new Error('Not implemented');
-}
+    indexes.forEach(function(curr, i){
+        arr = arr[indexes[i]]
+    });
+    return arr;
+ }
 
 
 /**
