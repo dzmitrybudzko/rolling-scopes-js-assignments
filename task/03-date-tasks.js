3e97d93b784f6127b4461225431ab24fc2aed498
Dzmitry Budzko1 <dzmitry_budzko1@epam.com>
2016-03-21 16:28:37 +0300

    fix #9

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -76,7 +76,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   return 
+    var delta = endDate - startDate;
+    return new Date(delta).toISOString().substring(11, -1);
 }
 
 
