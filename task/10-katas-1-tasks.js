5c0ab76ccbf1b6c117c841236c7ffea3c486029c
Dzmitry Budzko1 <dzmitry_budzko1@epam.com>
2016-03-24 10:19:43 +0300

    10-katas

--- task/10-katas-1-tasks.js
+++ task/10-katas-1-tasks.js
@@ -133,7 +133,26 @@ function* expandBraces(str) {
  *
  */
 function getZigZagMatrix(n) {
-    throw new Error('Not implemented');
+    
+    var matrix = Array.from({length: n}, x => Array.from({length: n}));   
+    var i = 1,
+        j = 1;
+
+    for (var e = 0; e < n*n; e++) {
+        matrix[i - 1][j - 1] = e;
+        if ((i + j) % 2 == 0) {
+            // Even diagonal
+            if (j < n) j ++;
+                else   i += 2;
+            if (i > 1) i --;
+        } else {
+            // Odd diagonal
+            if (i < n) i ++;
+                else   j += 2;
+            if (j > 1) j --;
+        }
+    }
+    return matrix;
 }
 
 
